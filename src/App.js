d4bc30076f62dfd218a2380025942d88f18c046e
James Truong <jtruong123@outlook.com>
2022-04-04 02:51:28 -0700

    add register page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Helmet } from 'react-helmet';
 import LoginPage from './pages/LoginPage';
+import RegisterPage from './pages/RegisterPage';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
 
       <Routes>
         <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
       </Routes>
     </Router>
   );
--- src/pages/RegisterPage.js
+++ src/pages/RegisterPage.js
@@ -1,7 +1,7 @@
 import { Link } from 'react-router-dom';
 import { useForm } from 'react-hook-form';
 
-function LoginPage() {
+function RegisterPage() {
   const { register, handleSubmit } = useForm();
 
   const onSubmit = (data) => console.log(data);
@@ -23,22 +23,40 @@ function LoginPage() {
           <div className="bg-white px-6 py-8 lg:rounded-xl lg:px-12 lg:shadow-lg">
             <div className="space-y-3">
               <h1 className="text-xl font-bold tracking-tight md:text-2xl">
-                Welcome back
+                Welcome
               </h1>
 
               <p className="text-sm text-gray-600">
-                Don't have an account?{' '}
+                Already have an account?{' '}
                 <Link
                   className="text-primary-600 transition hover:text-primary-500 focus:underline focus:outline-none"
-                  to="/register"
+                  to="/login"
                 >
-                  Sign up →
+                  Log in →
                 </Link>
               </p>
             </div>
 
             <form className="mt-6" onSubmit={handleSubmit(onSubmit)}>
-              <div className="mb-6 space-y-6">
+              <div className="mb-6 space-y-4">
+                <div className="space-y-2">
+                  <label
+                    htmlFor="name"
+                    className="inline-block text-sm font-medium leading-4 text-gray-700"
+                  >
+                    Full name
+                  </label>
+
+                  <div className="relative">
+                    <input
+                      id="name"
+                      type="text"
+                      className="block h-10 w-full rounded-lg border-gray-300 shadow-sm transition duration-75 focus:border-primary-600 focus:ring-1 focus:ring-inset focus:ring-primary-600"
+                      {...register('name', { required: true })}
+                    />
+                  </div>
+                </div>
+
                 <div className="space-y-2">
                   <label
                     htmlFor="email"
@@ -73,15 +91,24 @@ function LoginPage() {
                       {...register('password', { required: true })}
                     />
                   </div>
+                </div>
 
-                  <p className="text-sm text-gray-600">
-                    <Link
-                      className="text-primary-600 transition hover:text-primary-500 focus:underline focus:outline-none"
-                      to="/reset-password"
-                    >
-                      Forgot your password?
-                    </Link>
-                  </p>
+                <div className="space-y-2">
+                  <label
+                    htmlFor="passwordConfirmation"
+                    className="inline-block text-sm font-medium leading-4 text-gray-700"
+                  >
+                    Confirm password
+                  </label>
+
+                  <div className="relative">
+                    <input
+                      id="passwordConfirmation"
+                      type="password"
+                      className="block h-10 w-full rounded-lg border-gray-300 shadow-sm transition duration-75 focus:border-primary-600 focus:ring-1 focus:ring-inset focus:ring-primary-600"
+                      {...register('passwordConfirmation', { required: true })}
+                    />
+                  </div>
                 </div>
               </div>
 
@@ -89,7 +116,7 @@ function LoginPage() {
                 type="submit"
                 className="w-full justify-center rounded-lg border border-transparent bg-primary-600 py-1.5 px-4 font-medium tracking-tight text-white shadow transition-colors hover:bg-primary-500 focus:bg-primary-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white focus:ring-offset-2 focus:ring-offset-primary-700"
               >
-                Sign in
+                Sign up
               </button>
             </form>
           </div>
@@ -99,4 +126,4 @@ function LoginPage() {
   );
 }
 
-export default LoginPage;
+export default RegisterPage;
